00a884bc9c08d9f356fce9ec5fd1c9ae0efa3a7a
Pavan Kumar Anandam <Pavan.Anandam@snp.com>
2021-06-01 18:51:23 +0530

    Port reverted

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 var mysql = require('mysql');
-var port = process.env.port | 3003;
+var port = process.env.port;
 //var conn = mysql.createConnection({host: "samplemysql.mysql.database.azure.com", user: "azure_root@samplemysql", password: {your_password}, database: {your_database}, port: 3306, ssl:{ca:fs.readFileSync({ca-cert filename})}});
 
 var conn = mysql.createConnection({
